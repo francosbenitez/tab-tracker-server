afad19fe21e90503565027501aee907befad36c0
francosbenitez <francosbenitez@gmail.com>
2021-10-12 10:57:31 -0300

    feat: improved login & added token

--- src/controllers/AuthenticationController.js
+++ src/controllers/AuthenticationController.js
@@ -1,12 +1,26 @@
 //const { User } = require('../models')
 const db = require('../models');
 const User = db.user;
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
+function jwtSignUser (user) {
+    const ONE_WEEK = 60 * 60 * 24 * 7
+    return jwt.sign(user, config.authentication.jwtSecret, {
+      expiresIn: ONE_WEEK
+    })
+  }
 
 module.exports = {
     async register (req, res) {
         try {
             const user = await User.create(req.body)
-            res.send(user.toJSON())
+            //res.send(user.toJSON())
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson,
+                token: jwtSignUser(userJson)
+            })
         } catch (err) {
             res.status(400).send({
                 error: "This email account is already in use."
@@ -15,5 +29,40 @@ module.exports = {
         //res.send({
         //    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
         //})
+    },
+    async login (req, res) {
+        try {
+            const {email, password} = req.body
+            const user = await User.findOne({
+                where: {
+                    email: email
+                }
+            })
+            if (!user) {
+                return res.status(403).send({
+                    error: 'The login information was incorrect 1'
+                })
+            }
+
+            //const isPasswordValid = password === user.password
+            const isPasswordValid = await user.comparePassword(password)
+            console.log(password, isPasswordValid)
+            if (!isPasswordValid) {
+                return res.status(403).send({
+                    error: 'The login information was incorrect 2'
+                })
+            }
+            // const user = await User.create(req.body)
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson,
+                token: jwtSignUser(userJson)
+            })
+        } catch (err) {
+            //console.log(err)
+            res.status(500).send({
+                error: "An error has ocurred trying to log in"
+            })
+        }
     }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,21 @@
+const Promise = require('bluebird')
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
+
+function hashPassword (user, options) {
+  const SALT_FACTOR = 8
+
+  if (!user.changed('password')) {
+    return
+  }
+
+  return bcrypt
+    .genSaltAsync(SALT_FACTOR)
+    .then(salt => bcrypt.hashAsync(user.password, salt, null))
+    .then(hash => {
+      user.setDataValue('password', hash)
+    })
+}
+
 module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define('User', {
         email: {
@@ -5,6 +23,18 @@ module.exports = (sequelize, Sequelize) => {
             unique: true
         },
         password: Sequelize.STRING
-    })
+    }, {
+        hooks: {
+          beforeCreate: hashPassword,
+          beforeUpdate: hashPassword,
+          beforeSave: hashPassword
+        }
+      })
+    
+      User.prototype.comparePassword = function (password) {
+        console.log(this.password)
+        return bcrypt.compareAsync(password, this.password)
+    }
+
     return User
 }
